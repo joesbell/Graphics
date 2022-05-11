23f7e4cc885bded72937bd64b6296c7ed3f8910c
joesbell <joesbell@163.com>
2022-05-11 17:18:25 +0800

    提交

--- src/components/LayoutContent.vue
+++ src/components/LayoutContent.vue
@@ -1,13 +1,147 @@
-<script setup lang="ts">
-import { ref, reactive, computed, watch, onMounted } from 'vue';
+<template>
+  <div>
+     <div id="printMe" style="background:red;">
+         <!-- <el-table
+      
+      :data="tableData"
+      :span-method="arraySpanMethod"
+      border
+      style="width: 100%"
+    >
+      <el-table-column prop="id" label="ID" width="180" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="amount1" sortable label="Amount 1" />
+      <el-table-column prop="amount2" sortable label="Amount 2" />
+      <el-table-column prop="amount3" sortable label="Amount 3" />
+    </el-table> -->
+    
+     <el-table
+      :data="tableData"
+      :span-method="objectSpanMethod"
+      border
+      style="width: 100%; margin-top: 20px"
+    >
+      <el-table-column prop="id" label="ID" width="180" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="amount1" label="Amount 1" />
+      <el-table-column prop="amount2" label="Amount 2" />
+      <el-table-column prop="amount3" label="Amount 3" />
+    </el-table>
+    </div>
 
-// interface Props {
-//   foo: string
+     <button v-print="printObj">Print local range</button>
+
+   
+
+
+   
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { TableColumnCtx } from 'element-plus/es/components/table/src/table-column/defaults'
+import {
+  ref,
+  reactive,
+  toRefs,
+  computed,
+  watch,
+  watchEffect,
+  onMounted,
+} from 'vue';
+interface User {
+  id: string
+  name: string
+  amount1: string
+  amount2: string
+  amount3: number
+}
+
+interface SpanMethodProps {
+  row: User
+  column: TableColumnCtx<User>
+  rowIndex: number
+  columnIndex: number
+}
+
+// const arraySpanMethod = ({
+//   row,
+//   column,
+//   rowIndex,
+//   columnIndex,
+// }: SpanMethodProps) => {
+//   if (rowIndex % 2 === 0) {
+//     if (columnIndex === 0) {
+//       return [1, 2]
+//     } else if (columnIndex === 1) {
+//       return [0, 0]
+//     }
+//   }
 // }
 
-// const props = defineProps<Props>()
-</script>
+const objectSpanMethod = ({
+  row,
+  column,
+  rowIndex,
+  columnIndex,
+}: SpanMethodProps) => {
+  console.log(row);
+  
+  // if (columnIndex === 0) {
+  //   if (rowIndex % 2 === 0) {
+  //     return {
+  //       rowspan: 2,
+  //       colspan: 1,
+  //     }
+  //   } else {
+  //     return {
+  //       rowspan: 0,
+  //       colspan: 0,
+  //     }
+  //   }
+  // }
+}
+const printObj=ref({
+              id: "printMe",
+              popTitle: 'good print',
+              extraHead: '<meta http-equiv="Content-Language"content="zh-cn"/>',
 
-<template>
-  <div>content</div>
-</template>
+})
+const tableData: User[] = [
+  {
+    id: '12987122',
+    name: 'Tom',
+    amount1: '234',
+    amount2: '3.2',
+    amount3: 10,
+  },
+  {
+    id: '12987123',
+    name: 'Tom',
+    amount1: '165',
+    amount2: '4.43',
+    amount3: 12,
+  },
+  {
+    id: '12987122',
+    name: 'Tom',
+    amount1: '324',
+    amount2: '1.9',
+    amount3: 9,
+  },
+  {
+    id: '12987125',
+    name: 'Tom',
+    amount1: '621',
+    amount2: '2.2',
+    amount3: 17,
+  },
+  {
+    id: '12987126',
+    name: 'Tom',
+    amount1: '539',
+    amount2: '4.1',
+    amount3: 15,
+  },
+]
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
 import { Router } from './router';
+import ElementPlus from 'element-plus';
+import print from 'vue3-print-nb';
+import 'element-plus/dist/index.css';
 import App from './App.vue';
-createApp(App).use(Router).mount('#app');
+createApp(App).use(Router).use(ElementPlus).use(print).mount('#app');
