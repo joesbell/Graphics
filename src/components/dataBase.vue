9d697fdeff1148e35dbaf9920a609bebafb1244c
joesbell <joesbell@163.com>
2022-07-29 16:12:21 +0800

    修改eslint 错误

--- src/components/dataBase.vue
+++ src/components/dataBase.vue
@@ -7,7 +7,6 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <script setup lang="ts">
-import { ref, reactive, computed, onMounted } from 'vue';
 import { box, imgBox } from '@/assets/scss/imp.module.css';
 import wchat from '@/assets/WechatIMG1.jpeg?url';
 import dataJson from '@/assets/glob/data.json';
@@ -15,7 +14,7 @@ import MyWorker from '@/assets/worker?worker';
 import { useRoute, useRouter } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
-console.log(route.matched);
+console.log(route.matched, router);
 defineOptions({
   name: 'dataBase',
 });
